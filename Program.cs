1715f0149daedda2cb97ed76894c87d2859c0c46
HP <HP@DESKTOP-6SUN5DR>
2019-05-21 10:35:28 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace first_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hello/n");
+            Console.WriteLine(" WElcome to c#");
+        }
+    }
+}
